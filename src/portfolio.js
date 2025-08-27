c766b7ce1299e2b395e930f2ba70f7dc59211ce7
Lucian Zheng <zheng.young.zy@gmail.com>
2025-08-27 11:34:48 +1000

    feat: modify portfolio

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,10 +20,10 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "Lucian Zheng",
+  title: "Hi all, I'm Lucian",
   subTitle: emoji(
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
+    "A passionate Full Stack Software Developer 🚀 having experience in C++ embedded development, front-end development with React.js and Vue, back-end development with Rust, Python, and Java Spring, as well as smart contract development with Solidity."
   ),
   resumeLink:
     "https://drive.google.com/file/d/1ofFdKF_mqscH8WvXkSObnVvC9kK7Ldlu/view?usp=sharing", // Set to empty to hide the button
